2ef0939f9480b523505fd63ac6d72e3b1c70d8cd
Bruno Melano <melanobruno@gmail.com>
2025-08-17 02:42:00 -0300

    Update core.py

--- .github/workflows/files (20)/backend/app/chatbot/core.py
+++ .github/workflows/files (20)/backend/app/chatbot/core.py
@@ -18,3 +18,8 @@ async def get_chat_response(user_message: str) -> str:
         return completion.choices[0].message["content"]
     except Exception as e:
         return f"Error en la IA: {str(e)}"
+
+ModuleNotFoundError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app).
+Traceback:
+File "/mount/src/melania/.github/workflows/files (20)/backend/app/chatbot/core.py", line 2, in <module>
+    import openai
